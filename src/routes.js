06d86d0edee554458cb6e74788a6b36f41667528
BRUNA OLIVEIRA <91653767b@gmail.com>
2020-05-31 08:43:44 -0300

    parte do Front End

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import logon from './pages/logon';
+import register from './pages/register';
+import profile from './pages/profile';
+import newincident from './pages/newincident';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={logon} />
+        <Route path="/register" component={register} />
+
+        <Route path="/profile" component={profile} />
+        <Route path="/incidents/new" component={newincident} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
